55978e4c73c4deb8a4179d158576d24c4fc87454
zhangqingxin-daren <zhangqingxin@jldaren.com>
2014-08-13 13:46:35 +0800

    首页地图标注

--- modules.core/core-web-wicket/src/main/java/com/daren/core/web/wicket/HomePanel.java
+++ modules.core/core-web-wicket/src/main/java/com/daren/core/web/wicket/HomePanel.java
@@ -1,8 +1,17 @@
 package com.daren.core.web.wicket;
 
+import com.daren.rescue.api.biz.IRescueBeanService;
+import com.daren.rescue.entities.RescueBean;
+import com.google.gson.Gson;
+import org.apache.aries.blueprint.annotation.Reference;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.wicketstuff.shiro.ShiroConstraint;
 import org.wicketstuff.shiro.annotation.ShiroSecurityConstraint;
+
+import javax.inject.Inject;
+import java.util.List;
 
 /**
  * 项目名称:  urgent-project
@@ -15,9 +24,25 @@ import org.wicketstuff.shiro.annotation.ShiroSecurityConstraint;
  */
 @ShiroSecurityConstraint(constraint = ShiroConstraint.LoggedIn)
 public class HomePanel extends BasePanel {
+    //注入服务
+    @Inject
+    @Reference(id = "rescueBeanService", serviceInterface = IRescueBeanService.class)
+    private IRescueBeanService rescueBeanService;
+
     //ajax target container
     public HomePanel(String id, WebMarkupContainer wmc) {
         super(id, wmc);
 
+        //救援队标注
+        AjaxLink ajaxLinkRescue = new AjaxLink("rescueButton") {
+            @Override
+            public void onClick(AjaxRequestTarget ajaxRequestTarget) {
+                List<RescueBean> list = rescueBeanService.getAllEntity();
+                Gson gson = new Gson();
+                String string = gson.toJson(list);
+                ajaxRequestTarget.prependJavaScript("parseGson(" + string + ")");
+            }
+        };
+        this.add(ajaxLinkRescue);
     }
 }
