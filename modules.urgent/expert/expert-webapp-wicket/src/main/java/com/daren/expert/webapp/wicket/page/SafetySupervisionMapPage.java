698c34bcf42e12cb9c3991bfadf1399e9690f269
zhangqingxin-daren <zhangqingxin@jldaren.com>
2014-08-14 15:03:36 +0800

    专家模块标注地图

---
+++ modules.urgent/expert/expert-webapp-wicket/src/main/java/com/daren/expert/webapp/wicket/page/SafetySupervisionMapPage.java
@@ -0,0 +1,19 @@
+package com.daren.expert.webapp.wicket.page;
+
+import com.daren.core.web.wicket.component.dialog.IrisAbstractDialog;
+import com.daren.expert.entities.SafetySupervisionExpertBean;
+import org.apache.wicket.model.IModel;
+
+/**
+ * @类描述：专家管理-地图标注
+ * @创建人：张清欣
+ * @创建时间：2014-08-08 上午10:25
+ * @修改人：
+ * @修改时间：
+ * @修改备注：
+ */
+public class SafetySupervisionMapPage extends IrisAbstractDialog<SafetySupervisionExpertBean> {
+    public SafetySupervisionMapPage(String id, String title, IModel<SafetySupervisionExpertBean> model) {
+        super(id, title, model);
+    }
+}
