959aa2f5f89246f5bd185e0008cd3b23642fd44f
sunhao-daren <sunhao@jldaren.com>
2014-08-15 16:37:06 +0800

    change page style

--- modules.core/core-web-wicket/src/main/java/com/daren/core/web/wicket/HomePanel.java
+++ modules.core/core-web-wicket/src/main/java/com/daren/core/web/wicket/HomePanel.java
@@ -1,9 +1,6 @@
 package com.daren.core.web.wicket;
 
-import com.googlecode.wicket.kendo.ui.panel.KendoFeedbackPanel;
 import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.markup.html.pages.RedirectPage;
 import org.wicketstuff.shiro.ShiroConstraint;
 import org.wicketstuff.shiro.annotation.ShiroSecurityConstraint;
 
@@ -24,15 +21,6 @@ public class HomePanel extends BasePanel {
          * test forward link
          */
         super(id, wmc);
-        Link link = new Link("link") {
-            @Override
-            public void onClick() {
-                setResponsePage(new RedirectPage("http://www.baidu.com"));
-            }
-        };
-        add(link);
-        final KendoFeedbackPanel feedback = new KendoFeedbackPanel("feedback");
-        this.add(feedback);
 
 //        add(new Include("footer", "cus/Template/FooterPage.html"));
     }
