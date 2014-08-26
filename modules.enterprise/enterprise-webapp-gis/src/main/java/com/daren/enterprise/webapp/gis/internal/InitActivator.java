1143ec3ab5746e7e7f1050090bb9f36f93c834a9
donglongwen-daren <12345>
2014-08-26 08:16:02 +0800

    1.gis中在清新基础上修改地图, 2.功能包括危险点及详情展示,及资源搜索

---
+++ modules.enterprise/enterprise-webapp-gis/src/main/java/com/daren/enterprise/webapp/gis/internal/InitActivator.java
@@ -0,0 +1,35 @@
+package com.daren.enterprise.webapp.gis.internal;
+
+import org.apache.log4j.Logger;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpService;
+
+/**
+ * Created by sunlf on 14-3-23.
+ */
+public class InitActivator implements BundleActivator {
+
+    private static BundleContext context;
+    private static Logger logger = Logger.getLogger(InitActivator.class);
+    private ServiceReference reference;
+
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        logger.info("Start-up core web resources bundle!!");
+        context = bundleContext;
+        reference = bundleContext.getServiceReference(HttpService.class.getName());
+        HttpService httpService = (HttpService) bundleContext.getService(reference);
+//        httpService.registerResources("/custome/gis", "/custome/gis", null);
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        logger.info("Stop core web resources bundle!!");
+        if (reference != null)
+            bundleContext.ungetService(reference);
+        context = null;
+
+    }
+}
