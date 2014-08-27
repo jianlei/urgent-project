839ceed319915934e7e4623dc2e318d925db00b0
zhangqingxin-daren <zhangqingxin@jldaren.com>
2014-08-27 08:07:23 +0800

    Office控件整好，可能打开本地文档

---
+++ modules.example/example-webapp-wicket/src/main/java/com/daren/example/webapp/wicket/page/TestOfficePage.java
@@ -0,0 +1,13 @@
+package com.daren.example.webapp.wicket.page;
+
+import com.daren.core.web.wicket.BasePanel;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+
+/**
+ * Created by Administrator on 2014/8/26.
+ */
+public class TestOfficePage extends BasePanel {
+    public TestOfficePage(String id, WebMarkupContainer wmc) {
+        super(id, wmc);
+    }
+}
