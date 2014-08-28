4839f3ee8ef19901144681aaf6b006acc2e3d40d
zhangqingxin-daren <zhangqingxin@jldaren.com>
2014-08-28 13:44:05 +0800

    在线Office也封装好，在core包下component下的Office文件夹中，使用时请调用WindowOfficePage

---
+++ modules.example/example-webapp-wicket/src/main/resources/com/daren/example/webapp/wicket/page/LoadWebOffice.js
@@ -0,0 +1,4 @@
+var s = "";
+s += "<object id='WebOffice1' width='100%' height='500' classid='clsid:E77E049B-23FC-4DB8-B756-60529A35FAD5' codebase='../../cus/bundle/WebOffice6.cab#Version=6,0,4,4'>"
+s += "</OBJECT>";
+document.write(s);
