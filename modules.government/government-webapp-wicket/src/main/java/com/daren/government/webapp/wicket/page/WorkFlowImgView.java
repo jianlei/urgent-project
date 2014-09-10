6860efde96d1a33c1419465030d6b434f8179742
donglongwen-daren <12345>
2014-09-10 19:11:10 +0800

    流程图查看--目前 processDefinitionId 和processInstaceId 都是死的明天替换

--- modules.government/government-webapp-wicket/src/main/java/com/daren/government/webapp/wicket/page/WorkFlowImgView.java
+++ modules.government/government-webapp-wicket/src/main/java/com/daren/government/webapp/wicket/page/WorkFlowImgView.java
@@ -1,17 +1,13 @@
 package com.daren.government.webapp.wicket.page;
 
-import com.daren.government.webapp.wicket.css.JScriptReferences;
-import org.apache.wicket.markup.head.JavaScriptHeaderItem;
-import org.apache.wicket.markup.html.internal.HtmlHeaderContainer;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.request.resource.PackageResourceReference;
 
 public class WorkFlowImgView extends Panel {
     public WorkFlowImgView(String id,String flwid) {
         super(id);
     }
 
-    @Override
+/*    @Override
     public void renderHead(HtmlHeaderContainer container) {
        container.getHeaderResponse().render(JavaScriptHeaderItem.forReference(new PackageResourceReference(JScriptReferences.class, "js/jstools.js")));
         container.getHeaderResponse().render(JavaScriptHeaderItem.forReference(new PackageResourceReference(JScriptReferences.class, "js/raphael.js")));
@@ -32,5 +28,5 @@ public class WorkFlowImgView extends Panel {
         //container.getHeaderResponse().render(CssHeaderItem.forReference(new PackageResourceReference(JScriptReferences.class, "js/bootstrap-1.0.0.css")));
 
         super.renderHead(container);
-    }
+    }*/
 }
