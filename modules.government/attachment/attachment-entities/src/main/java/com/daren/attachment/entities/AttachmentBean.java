0600eb68b93e938d45cba573d6bdb528ed1d783b
zhangqingxin-daren <zhangqingxin@jldaren.com>
2014-09-12 16:41:08 +0800

    行政审批上传功能  完成

---
+++ modules.government/attachment/attachment-entities/src/main/java/com/daren/attachment/entities/AttachmentBean.java
@@ -0,0 +1,67 @@
+package com.daren.attachment.entities;
+
+import com.daren.core.api.persistence.PersistentEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+import java.util.Date;
+
+/**
+ * @类描述：审批附件
+ * @创建人：张清欣
+ * @创建时间：2014-09-09 下午14:50
+ * @修改人：
+ * @修改时间：
+ * @修改备注：
+ */
+@Entity
+@Table(name = "gov_attachment")
+public class AttachmentBean extends PersistentEntity {
+    private String name;//文件名称
+    private String path;//文件路径
+    private String type;//文件类型
+    private String appType;//审批类型
+    private long parentId;//审批ID
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAppType() {
+        return appType;
+    }
+
+    public void setAppType(String appType) {
+        this.appType = appType;
+    }
+
+    public long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(long parentId) {
+        this.parentId = parentId;
+    }
+}
