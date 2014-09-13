e081187e3d90510ee7f435f718a3ffb3a4dd93f9
wangkairan-daren <www.163.com>
2014-09-13 16:48:34 +0800

    完善第三方流程启动界面

---
+++ modules.government/apply/competency-webapp-wicket/src/main/java/com/daren/apply/webapp/wicket/page/NotFoundProcess.java
@@ -0,0 +1,14 @@
+package com.daren.workflow.web.bootup.wicket;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ * Created by Dell on 14-9-13.
+ */
+public class NotFoundProcess extends Panel {
+
+    public NotFoundProcess(String id){
+        super(id);
+    }
+}
