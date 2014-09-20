f351b4fa37b8dc4b79849a5c9498827b196834fe
sunlingfeng-daren <sunlingfeng@jldaren.com>
2014-09-20 09:34:25 +0800

    增加growl和lightbox2个组件，并增加相应的测试代码

---
+++ modules.core/core-web-jgrowl/src/main/java/com/daren/core/web/component/jgrowl/FunctionString.java
@@ -0,0 +1,36 @@
+package com.daren.core.web.component.jgrowl;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * @author Edvin Syse <edvin@sysedata.no>
+ * 
+ */
+public class FunctionString implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+	private String value;
+
+	public FunctionString(String value)
+	{
+		this.value = value;
+	}
+
+	public String getValue()
+	{
+		return value;
+	}
+
+	public void setValue(String value)
+	{
+		this.value = value;
+	}
+
+	@Override
+	public String toString()
+	{
+		return value;
+	}
+
+}
